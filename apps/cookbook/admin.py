1df4fcff320d06c9c1472fb841643636038d3df7
Steven Robyns <robynssteven@gmail.com>
2019-05-29 07:23:14 +0200

    Creation of cookbook model structure

--- apps/cookbook/admin.py
+++ apps/cookbook/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Course, Label, Cuisine, Colour, Recipe, Step, Ingredient, Unit, IngredientList, RecipeLabel
+
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Label)
+admin.site.register(Cuisine)
+admin.site.register(Colour)
+admin.site.register(Recipe)
+admin.site.register(Step)
+admin.site.register(Ingredient)
+admin.site.register(Unit)
+admin.site.register(IngredientList)
+admin.site.register(RecipeLabel)
--- apps/cookbook/models.py
+++ apps/cookbook/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+
+class Cuisine(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    servings = models.IntegerField()
+    preparation_time = models.IntegerField()
+    cost = models.IntegerField()
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    cuisine_id = models.ForeignKey(Cuisine, on_delete=models.CASCADE)
+    # TODO: to implement image later in this model
+
+    def __str__(self):
+        return self.name
+
+
+class Step(models.Model):
+    id = models.AutoField(primary_key=True)
+    step_number = models.IntegerField()
+    instruction = models.TextField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('recipe', 'step_number')
+
+    def __str__(self):
+        return self.recipe.name + " - Step " + str(self.step_number)
+
+
+class Ingredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Unit(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientList(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2)
+    unit = models.ForeignKey(Unit, blank=True, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ('recipe', 'ingredient')
+
+
+class Colour(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    hex = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Label(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    colour = models.ForeignKey(
+        Colour, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeLabel(models.Model):
+    id = models.AutoField(primary_key=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('recipe', 'label')
